8e6f08b30fa301a9c247cd554308aed1446243a1
小泉博史 <hirofumi.koizumi@gmail.com>
2026-06-24 11:08:56 +0900

    Add subscription plan limits

---
+++ src/constants/plans.ts
@@ -0,0 +1,14 @@
+export const FREE_PLAN_LIMITS = {
+  dailyChatMessages: 5,
+  contentItems: 10,
+  dailyQuizQuestions: 5,
+  favorites: 10,
+};
+
+export const PREMIUM_PRODUCT_ID = 'pocket_senpai_monthly_500';
+
+export const PREMIUM_PLAN = {
+  name: 'ポケット先輩プレミアム',
+  priceLabel: '月額500円',
+  productId: PREMIUM_PRODUCT_ID,
+};
